1fb865d1d8ecd97b8ed0e9f600c0907fc0058411
Diego <diego@kambista.com>
2020-01-05 15:35:47 -0500

    Updates

---
+++ precache-manifest.b60d13369f865545e10f1ccfc0f5b12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6655b8dcc52d706a88773a0ef749c5",
+    "url": "/tusadiandra/index.html"
+  },
+  {
+    "revision": "62d22434e005872f6473",
+    "url": "/tusadiandra/static/css/main.0410c637.chunk.css"
+  },
+  {
+    "revision": "08d5b5b25797a2a15bec",
+    "url": "/tusadiandra/static/js/2.d014e299.chunk.js"
+  },
+  {
+    "revision": "d69d7e9a41cabec9c8990d409922ef43",
+    "url": "/tusadiandra/static/js/2.d014e299.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62d22434e005872f6473",
+    "url": "/tusadiandra/static/js/main.df94b136.chunk.js"
+  },
+  {
+    "revision": "c6a0e3057b87d60f283e",
+    "url": "/tusadiandra/static/js/runtime-main.868accdd.js"
+  }
+]);
